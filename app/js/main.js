1361faff9431588187e13960164d3b76ebbb94c7
Kirill2210 <kirill.konovalov.s3@gmail.com>
2021-07-21 12:47:52 +0500

    end header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
 });
+
+document.querySelector('.header__top-search-btn').onclick = function() {
+  document.querySelector('.header__top-search').classList.toggle('active');
+}
+
+$('.header__slider').slick({
+  arrows: false,
+  dots: true
+});
